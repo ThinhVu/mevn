38071ed169b790ab479715818afeeb3a2aae4cc4
ThinhVu <vutrongthinhk7@gmail.com>
2023-03-18 16:28:46 +0700

    Add constants

---
+++ fe/src/constants.js
@@ -0,0 +1,7 @@
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://api.url' : 'http://localhost:4000';
+export const HMM_API_URL = `${API_URL}/hmm`;
+export const FILE_API_URL = 'http://localhost:8081/api'
+export const SOCKET_EVENTS = {
+
+}
+
