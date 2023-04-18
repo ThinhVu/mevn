0b2bd3c8699f1f4f9fcd62f190a182b6844aa388
ThinhVu <vutrongthinhk7@gmail.com>
2023-04-18 15:13:10 +0700

    chore: Overlay -> script setup

--- fe/src/components/UiLib/Layer/Overlay.vue
+++ fe/src/components/UiLib/Layer/Overlay.vue
@@ -1,23 +1,18 @@
-<script lang="jsx">
-export default {
-  name: 'Overlay',
-  props: {zIndex: Number, raiseEvent: Boolean},
-  emits: ['click'],
-  setup(props, ctx) {
-    const handleOnClick = (e) => {
-      if (props.raiseEvent) {
-        ctx.emit('click', e);
-      } else {
-        e.preventDefault();
-        e.stopPropagation();
-      }
-    }
-    return () => <div
-        class="w-100vw h-100vh fr ai-c fix top-0 left-0"
-        style={{zIndex: props.zIndex}}
-        onClick={handleOnClick}>
-      {ctx.slots.default && ctx.slots.default()}
-    </div>
+<script setup>
+const props = defineProps({zIndex: Number, raiseEvent: Boolean})
+const emit = defineEmits(['click'])
+const handleOnClick = (e) => {
+  if (props.raiseEvent) {
+    emit('click', e);
+  } else {
+    e.preventDefault();
+    e.stopPropagation();
   }
 }
 </script>
+
+<template>
+  <div class="w-100vw h-100vh fr ai-c fix top-0 left-0" :style="{zIndex}" @click="handleOnClick">
+    <slot></slot>
+  </div>
+</template>
