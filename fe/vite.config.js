b3765d4008f634fe3b1218b6f6b2f0d9947a36a2
ThinhVu <vutrongthinhk7@gmail.com>
2023-06-09 22:54:41 +0700

    remove comment

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,9 +1,8 @@
+import css from 'jinguji/src/styles/tvuxcss'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from "@vitejs/plugin-vue-jsx"
 import UnoCSS from 'unocss/vite'
 import Inspect from 'vite-plugin-inspect'
-// import {presetUno} from 'unocss';
-import tvuxCss from 'jinguji/src/styles/tvuxcss'
 import path from 'path';
 
 export default {
@@ -11,13 +10,7 @@ export default {
     vue(),
     vueJsx({}),
     Inspect(),
-    UnoCSS({
-      /* https://github.com/unocss/unocss#using-presets */
-      presets: [
-        // presetUno()
-      ],
-      rules: tvuxCss
-    })
+    UnoCSS({rules: css})
   ],
   resolve: {
     alias: {
