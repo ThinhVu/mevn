80b97b5c60ae30ec0091ca7a8531a188be9329cc
ThinhVu <vutrongthinhk7@gmail.com>
2023-06-09 22:55:41 +0700

    set empty presets

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from "@vitejs/plugin-vue-jsx"
 import UnoCSS from 'unocss/vite'
 import Inspect from 'vite-plugin-inspect'
+
 import path from 'path';
 
 export default {
@@ -10,7 +11,10 @@ export default {
     vue(),
     vueJsx({}),
     Inspect(),
-    UnoCSS({rules: css})
+    UnoCSS({
+      presets: [],
+      rules: css
+    })
   ],
   resolve: {
     alias: {
