d6b536b04d62179b9e6e6bd02ed37b71afac6e07
ThinhVu <vutrongthinhk7@gmail.com>
2023-07-13 01:07:33 +0700

    update env-var: PM2

--- be/ecosystem.config.js
+++ be/ecosystem.config.js
@@ -2,5 +2,5 @@ module.exports = [{
   script: './dist/index.js',
   name: 'app',
   exec_mode: 'cluster',
-  instances: process.env.RUNNING_INSTANCES === undefined ? 0 : +process.env.RUNNING_INSTANCES
+  instances: process.env.PM2_RUNNING_INSTANCES === undefined ? 0 : +process.env.RUNNING_INSTANCES
 }]
