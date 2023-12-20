8510c77da35699186f72250572aa49897d090d31
ThinhVu <thinhvux@protonmail.com>
2023-12-21 00:32:53 +0700

    remove pm2

--- be/ecosystem.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = [{
-  script: './dist/index.js',
-  name: 'app',
-  exec_mode: 'cluster',
-  instances: process.env.PM2_RUNNING_INSTANCES === undefined ? 0 : +process.env.PM2_RUNNING_INSTANCES
-}]
